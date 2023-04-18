4a3b905cd061f37c47ba78898c52bdbb502d89fd
faforus <filip.wielechowski@gmail>
2023-04-18 11:29:26 +0200

    1.0

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,10 @@
 import classes from "./Contact.module.css";
-import Title from "../components/Title";
 import ContactForm from "../components/ContactForm";
 import { Fragment } from "react";
 
 const Contact = () => {
   return (
     <Fragment>
-      <Title title={"Kontakt"} />
       <div className={classes["contact-container"]}>
         <div className={classes["contact-image"]}></div>
         <div className={classes["contact-form"]}>
